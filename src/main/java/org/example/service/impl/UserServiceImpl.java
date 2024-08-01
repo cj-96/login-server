ce87482692252acba9cc6ef820e965d7c20654db
cj-96 <chamfer1996@gmail.com>
2024-08-01 21:56:44 +0530

    user repository layer

--- src/main/java/org/example/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -1,9 +1,19 @@
 package org.example.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.example.dto.User;
+import org.example.entity.UserEntity;
+import org.example.repository.UserRepository;
 import org.example.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    final UserRepository repository;
+    final ObjectMapper mapper;
     @Override
     public boolean login(User user) {
         return false;
@@ -11,6 +21,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String register(User user) {
+        repository.save(mapper.convertValue(user, UserEntity.class));
         return "";
     }
 }
